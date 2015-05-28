103c9f9249c1739c5d55f02d0224a546730b61a1
Josh Marchello <jraymarchello@gmail.com>
2015-05-27 22:31:30 -0600

    Implemented FileSorter::find_css and FileSorter::find_js

--- test/test_file_sorter.rb
+++ test/test_file_sorter.rb
@@ -4,38 +4,38 @@ require "file_sorter.rb"
 class FileSorterTest < Minitest::Test
   def setup
     @vender_dir = "test_app/app/vender"
-    @theme_orig = "theme/"
+    @theme_orig = "test/theme/"
   end
 
   def test_find_css
-    file_sorter = FileSorter.new
-    file_sorter.find_css(@theme_orig)
+    file_sorter = FileSorter.new(@theme_orig)
+    file_sorter.find_css
     css_files = file_sorter.instance_variable_get(:@css_files)
     refute_empty(css_files)
   end
 
   def test_find_css_returns_only_css
-    file_sorter = FileSorter.new
-    file_sorter.find_css(@theme_orig)
+    file_sorter = FileSorter.new(@theme_orig)
+    file_sorter.find_css
     css_files = file_sorter.instance_variable_get(:@css_files)
     css_files.each do |file|
-      assert(File.extname(file) == ".css" || File.extname(file) == ".scss")
+      assert(File.extname(file) == ".css" || File.extname(file) == ".scss", "file #{file} is not a css or scss file")
     end
   end
 
   def test_find_js
-    file_sorter = FileSorter.new
-    file_sorter.find_css(@theme_orig)
+    file_sorter = FileSorter.new(@theme_orig)
+    file_sorter.find_js
     js_files = file_sorter.instance_variable_get(:@js_files)
     refute_empty(js_files)
   end
 
   def test_find_js_returns_only_js
-    file_sorter = FileSorter.new
-    file_sorter.find_css(@theme_orig)
+    file_sorter = FileSorter.new(@theme_orig)
+    file_sorter.find_js
     js_files = file_sorter.instance_variable_get(:@js_files)
     js_files.each do |file|
-      assert(File.extname(file) == ".js" || File.extname(file) == ".coffee")
+      assert(File.extname(file) == ".js" || File.extname(file) == ".coffee", "file #{file} is not a js or coffee file")
     end
   end
 end
