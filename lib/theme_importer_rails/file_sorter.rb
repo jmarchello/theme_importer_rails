7813dd037080fe55e25c1e1ac85f2c5d79c6b30f
Josh Marchello <jraymarchello@gmail.com>
2015-06-02 02:09:43 -0600

    Implemented methods to find and sort fonts and images

--- lib/theme_importer_rails/file_sorter.rb
+++ lib/theme_importer_rails/file_sorter.rb
@@ -6,11 +6,17 @@ class FileSorter
   @theme_orig
   @css_files
   @js_files
+  @font_files
+  @images
+  @font_extensions
+  @image_extensions
 
   public
 
   def initialize(theme_orig)
     @theme_orig = theme_orig
+    @font_extensions = [".eot", ".svg", ".ttf", ".woff", ".otf", ".woff2"]
+    @image_extensions = [".png", ".jpg", ".jpeg", ".gif"]
   end
 
   def find_css
@@ -21,7 +27,16 @@ class FileSorter
     @js_files = Dir.glob("#{@theme_orig}/**/*{.js,.coffee}")
   end
 
+  def find_font_files
+    @font_files = Dir.glob("#{@theme_orig}/**/*{#{@font_extensions.join(',')}}")
+  end
+
+  def find_images
+    @images = Dir.glob("#{@theme_orig}/**/*{#{@image_extensions.join(',')}}")
+  end
+
   def create_imported_theme_dir
+    # vender assets directory for css and js files
     if !Dir.exist?("#{Rails.root}/vender/assets")
       Dir.mkdir("#{Rails.root}/vender/assets")
     end
@@ -29,56 +44,103 @@ class FileSorter
     if !Dir.exist?("#{Rails.root}/vender/assets/imported_theme")
       Dir.mkdir("#{Rails.root}/vender/assets/imported_theme")
     end
+
+    #public assets directory for fonts and images
+    if !Dir.exist?("#{Rails.root}/public/assets")
+      Dir.mkdir("#{Rails.root}/public/assets")
+    end
+
+    if !Dir.exist?("#{Rails.root}/public/assets/imported_theme")
+      Dir.mkdir("#{Rails.root}/public/assets/imported_theme")
+    end
   end
 
   def move_css_files
-    create_imported_theme_dir
+    unless @css_files.nil?
+      create_imported_theme_dir
 
-    if !Dir.exist?("#{Rails.root}/vender/assets/imported_theme/stylesheets")
-      Dir.mkdir("#{Rails.root}/vender/assets/imported_theme/stylesheets")
-    end
+      if !Dir.exist?("#{Rails.root}/vender/assets/imported_theme/stylesheets")
+        Dir.mkdir("#{Rails.root}/vender/assets/imported_theme/stylesheets")
+      end
 
-    @css_files.each do |file|
-      file_name = File.basename(file)
-      FileUtils.cp(file, "#{Rails.root}/vender/assets/imported_theme/stylesheets/#{file_name}")
+      @css_files.each do |file|
+        file_name = File.basename(file)
+        FileUtils.cp(file, "#{Rails.root}/vender/assets/imported_theme/stylesheets/#{file_name}")
+      end
     end
   end
 
   def move_js_files
-    create_imported_theme_dir
+    unless @js_files.nil?
+      create_imported_theme_dir
 
-    if !Dir.exist?("#{Rails.root}/vender/assets/imported_theme/javascripts")
-      Dir.mkdir("#{Rails.root}/vender/assets/imported_theme/javascripts")
+      if !Dir.exist?("#{Rails.root}/vender/assets/imported_theme/javascripts")
+        Dir.mkdir("#{Rails.root}/vender/assets/imported_theme/javascripts")
+      end
+
+      @js_files.each do |file|
+        file_name = File.basename(file)
+        FileUtils.cp(file, "#{Rails.root}/vender/assets/imported_theme/javascripts/#{file_name}")
+      end
     end
+  end
 
-    @js_files.each do |file|
-      file_name = File.basename(file)
-      FileUtils.cp(file, "#{Rails.root}/vender/assets/imported_theme/javascripts/#{file_name}")
+  def move_font_files
+    unless @font_files.nil?
+      create_imported_theme_dir
+
+      if !Dir.exist?("#{Rails.root}/public/assets/imported_theme/fonts")
+        Dir.mkdir("#{Rails.root}/public/assets/imported_theme/fonts")
+      end
+
+      @font_files.each do |file|
+        file_name = File.basename(file)
+        FileUtils.cp(file, "#{Rails.root}/public/assets/imported_theme/fonts/#{file_name}")
+      end
+    end
+  end
+
+  def move_images
+    unless @images.nil?
+      create_imported_theme_dir
+
+      if !Dir.exist?("#{Rails.root}/public/assets/imported_theme/images")
+        Dir.mkdir("#{Rails.root}/public/assets/imported_theme/images")
+      end
+
+      @images.each do |file|
+        file_name = File.basename(file)
+        FileUtils.cp(file, "#{Rails.root}/public/assets/imported_theme/images/#{file_name}")
+      end
     end
   end
 
   def generate_css_manifest
-    create_imported_theme_dir
+    unless @css_files.nil?
+      create_imported_theme_dir
 
-    manifest_file = File.open("#{Rails.root}/vender/assets/imported_theme/imported_theme.css", "w")
-    manifest_file.puts("/*")
-    @css_files.each do |css_file|
-      css_file_name = File.basename(css_file)
-      manifest_file.puts("*= require stylesheets/#{css_file_name}")
+      manifest_file = File.open("#{Rails.root}/vender/assets/imported_theme/imported_theme.css", "w")
+      manifest_file.puts("/*")
+      @css_files.each do |css_file|
+        css_file_name = File.basename(css_file)
+        manifest_file.puts("*= require stylesheets/#{css_file_name}")
+      end
+      manifest_file.puts("*/")
+      manifest_file.close
     end
-    manifest_file.puts("*/")
-    manifest_file.close
   end
 
   def generate_js_manifest
-    create_imported_theme_dir
+    unless @js_files.nil?
+      create_imported_theme_dir
 
-    manifest_file = File.open("#{Rails.root}/vender/assets/imported_theme/imported_theme.js", "w")
-    @js_files.each do |js_file|
-      js_file_name = File.basename(js_file)
-      manifest_file.puts("//= require javascripts/#{js_file_name}")
+      manifest_file = File.open("#{Rails.root}/vender/assets/imported_theme/imported_theme.js", "w")
+      @js_files.each do |js_file|
+        js_file_name = File.basename(js_file)
+        manifest_file.puts("//= require javascripts/#{js_file_name}")
+      end
+      manifest_file.close
     end
-    manifest_file.close
   end
 
 end
--- test/file_sorter_test.rb
+++ test/file_sorter_test.rb
@@ -3,13 +3,19 @@ require "fileutils"
 
 class FileSorterTest < ActiveSupport::TestCase
   def setup
-    @theme_orig = "test/theme/"
+    @theme_orig = "test/theme"
     @file_sorter = FileSorter.new(@theme_orig)
+    @font_extensions = @file_sorter.instance_variable_get(:@font_extensions)
+    @image_extensions = @file_sorter.instance_variable_get(:@image_extensions)
   end
 
   def teardown
     if Dir.exist?("#{Rails.root}/vender/assets/imported_theme")
       FileUtils.rm_rf("#{Rails.root}/vender/assets/imported_theme")
+    end
+
+    if Dir.exist?("#{Rails.root}/public/assets/imported_theme")
+      FileUtils.rm_rf("#{Rails.root}/public/assets/imported_theme")
     end
   end
 
@@ -43,8 +49,10 @@ class FileSorterTest < ActiveSupport::TestCase
 
   def test_create_imported_theme_dir
     @file_sorter.create_imported_theme_dir
-    assert(Dir.exists?("#{Rails.root}/vender/assets"))
-    assert(Dir.exists?("#{Rails.root}/vender/assets/imported_theme"))
+    assert(Dir.exists?("#{Rails.root}/vender/assets"), "vender/assets does not exist.")
+    assert(Dir.exists?("#{Rails.root}/vender/assets/imported_theme"), "vender/assets/imported_theme does not exist.")
+    assert(Dir.exists?("#{Rails.root}/public/assets"), "public/assets does not exist.")
+    assert(Dir.exists?("#{Rails.root}/public/assets/imported_theme"), "public/assert/imported_theme does not exist.")
   end
 
   def test_move_css_files
@@ -77,5 +85,43 @@ class FileSorterTest < ActiveSupport::TestCase
     @file_sorter.find_js
     @file_sorter.generate_js_manifest
     assert(FileUtils.compare_file("test/model_js_manifest.js" ,"#{Rails.root}/vender/assets/imported_theme/imported_theme.js"))
+  end
+
+  def test_find_fonts_files
+    @file_sorter.find_font_files
+    font_files = @file_sorter.instance_variable_get(:@font_files)
+    refute_empty(font_files)
+    font_files.each do |font_file|
+      assert(@font_extensions.include? File.extname(font_file))
+    end
+  end
+
+  def test_move_font_files
+    @file_sorter.find_font_files
+    @file_sorter.move_font_files
+    font_files = @file_sorter.instance_variable_get(:@font_files)
+    font_files.each do |font_file|
+      file_name = File.basename(font_file)
+      assert(File.exists?("#{Rails.root}/public/assets/imported_theme/fonts/#{file_name}"))
+    end
+  end
+
+  def test_find_images
+    @file_sorter.find_images
+    images = @file_sorter.instance_variable_get(:@images)
+    refute_empty(images)
+    images.each do |image|
+      assert(@image_extensions.include? File.extname(image))
+    end
+  end
+
+  def test_move_images
+    @file_sorter.find_images
+    @file_sorter.move_images
+    images = @file_sorter.instance_variable_get(:@images)
+    images.each do |image|
+      file_name = File.basename(image)
+      assert(File.exists?("#{Rails.root}/public/assets/imported_theme/images/#{file_name}"))
+    end
   end
 end
